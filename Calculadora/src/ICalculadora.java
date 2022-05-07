15830e9a77d6f6f7d1c8d75608428b08635b42f0
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-05-07 19:39:18 +0200

    docs: Aplicar JavaDoc a la Interfaz ICalculadora

--- Calculadora/src/ICalculadora.java
+++ Calculadora/src/ICalculadora.java
@@ -1,11 +1,40 @@
+/**
+ * Interfaz ICalculadora que contiene los métodos de las operaciones básicas de una calculadora
+ * @author Marcos López
+ * @author Lisbeth Poaquiza
+ * @since 07-05-2022
+ * @version 1.0.0
+ */
 public interface ICalculadora {
-
+    /**
+     * Realizar una operación aritmética que reune varias cantidades en una sola.
+     * @param num1 primer entero a sumar
+     * @param num2 segundo entero a sumar
+     * @return entero que representa la suma de los parámetros
+     */
     public int sumar(int num1, int num2);
 
+    /**
+     * Realiza una operación aritmética que quita una cantidad (sustraendo) de otra (minuendo) para averiguar la diferencia entre las dos.
+     * @param num1 entero que representa al minuendo
+     * @param num2 entero que representa al sustraendo
+     * @return entero que representa la diferencia de los parámetros
+     */
     public int restar(int num1, int num2);
 
+    /**
+     * Aumentar el número o la cantidad de cosas de la misma especie
+     * @param num1 primer entero a multiplicar
+     * @param num2 segundo entero a multiplicar
+     * @return entero que representa el producto de la multiplicación de los parámetros
+     */
     public int multiplicar(int num1, int num2);
 
+    /**
+     *Averigua cuántas veces una cantidad, llamada dividendo, contiene a otra, llamada divisor
+     * @param num1 entero que representa al dividendo
+     * @param num2 entero que representa al divisor
+     * @return entero que representa el cociente de la división de los parámetros
+     */
     public int dividir(int num1, int num2);
-
 }

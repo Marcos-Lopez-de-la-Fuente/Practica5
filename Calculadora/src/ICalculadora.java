a5a650747a7ff7beff0f2012d3b3c6c96d268b22
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-05-07 19:33:29 +0200

    feat: Creación Interfaz ICalculadora con métodos

---
+++ Calculadora/src/ICalculadora.java
@@ -0,0 +1,11 @@
+public interface ICalculadora {
+
+    public int sumar(int num1, int num2);
+
+    public int restar(int num1, int num2);
+
+    public int multiplicar(int num1, int num2);
+
+    public int dividir(int num1, int num2);
+
+}
